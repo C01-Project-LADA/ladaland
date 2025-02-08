a11c6a9db924493b9a58cd8b68668f25e16bef69
Andrey Zinovyev <andrey.znvyev@gmail.com>
2025-02-07 21:52:27 -0500

    Change auth from jwt to express session

--- core/src/index.ts
+++ core/src/index.ts
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 import registerRoute from './routes/user/register';
 import loginRoute from './routes/user/login';
 import visaRequirementsRoute from './routes/visa-requirements';
-
-dotenv.config();
-
+import session from 'express-session';
 import express from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
+
+dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
@@ -15,9 +15,20 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'default',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
+
 app.use('/api', registerRoute);
 app.use('/api', loginRoute);
-app.use('/api', visaRequirementsRoute)
+app.use('/api', visaRequirementsRoute);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
