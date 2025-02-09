a58f8dc5052c943162f9baa941da5abae4bc4d81
Andrey Zinovyev <andrey.znvyev@gmail.com>
2025-02-09 17:32:32 -0500

    Implement AI-based suggestions

--- core/src/index.ts
+++ core/src/index.ts
@@ -3,6 +3,7 @@ import registerRoute from './routes/user/register';
 import loginRoute from './routes/user/login';
 import visaRequirementsRoute from './routes/visa-requirements';
 import visitedCountriesPercentRoute from './routes/visitedCountriesPercent';
+import travelSuggestionsRoute from './routes/travel-suggestions';
 import session from 'express-session';
 import express from 'express';
 import cors from 'cors';
@@ -31,6 +32,7 @@ app.use('/api', registerRoute);
 app.use('/api', loginRoute);
 app.use('/api', visaRequirementsRoute);
 app.use('/api', visitedCountriesPercentRoute);
+app.use('/api', travelSuggestionsRoute);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
