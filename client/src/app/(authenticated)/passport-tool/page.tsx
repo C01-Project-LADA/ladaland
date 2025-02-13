ab1cc66c251a87bcdd0f158b15b9299db84e147f
Andre Fong <fongandre123@gmail.com>
2025-02-13 15:35:48 -0500

    chore: Passport tool page skeleton

--- client/src/app/(authenticated)/passport-tool/page.tsx
+++ client/src/app/(authenticated)/passport-tool/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import PassportChecker from '@/components/PassportChecker';
+import styles from '@/styles/PassportTool.module.css';
 
 export default function PassportTool() {
   return (
-    <div>
-      <div className="mb-[1000px]">PassportTool</div>
+    <div className={styles.container}>
+      <div className={styles.header_block}>
+        <h2>PASSPORT TOOL</h2>
+        <h1>Check if you need a visa for your next destination!</h1>
+      </div>
+
+      <h2 className={styles.title}>I have a passport from...</h2>
+
+      <PassportChecker />
     </div>
   );
 }
---
+++ client/src/components/PassportChecker.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function PassportChecker() {
+  return (
+    <>
+      <div>Passport Checker</div>
+    </>
+  );
+}
