d12ba97d4e9b0fae0b7ba0a96d5b26e7d6c5e4e8
Andre Fong <fongandre123@gmail.com>
2025-02-13 20:39:02 -0500

    feat: Add country select modal

---
+++ client/src/components/Pagination.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+export default function Pagination({
+  page,
+  pageCount,
+  setPage,
+}: {
+  page: number;
+  pageCount: number;
+  setPage: (page: number) => void;
+}) {
+  return (
+    <div className="w-full justify-center flex items-center gap-6">
+      <Button
+        variant="outline"
+        size="icon"
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+      >
+        <ChevronLeft />
+      </Button>
+      <p className="text-lg font-semibold">{page}</p>
+      <Button
+        variant="outline"
+        size="icon"
+        disabled={page === pageCount}
+        onClick={() => setPage(page + 1)}
+      >
+        <ChevronRight />
+      </Button>
+    </div>
+  );
+}
--- client/src/components/PassportChecker.tsx
+++ client/src/components/PassportChecker.tsx
@@ -1,9 +1,140 @@
 'use client';
 
+import styles from '@/styles/PassportChecker.module.css';
+import { Button } from '@/components/ui/button';
+import { Plus, X, Search, Check } from 'lucide-react';
+import {
+  AlertDialog,
+  // AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  // AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import Pagination from '@/components/Pagination';
+import { Input } from '@/components/ui/input';
+import { useEffect, useState } from 'react';
+import useCountries from '@/hooks/useCountries';
+import Image from 'next/image';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+
 export default function PassportChecker() {
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [passportSearchOpen, setPassportSearchOpen] = useState(false);
+
+  const [hasPassport, setHasPassport] = useState<Record<string, boolean>>({});
+
+  // Reset page when search changes
+  useEffect(() => {
+    setPage(1);
+  }, [search]);
+
+  const { countries, pageCount } = useCountries({
+    filter: search,
+    page,
+  });
+
+  function handleOpenChange(open: boolean) {
+    setPassportSearchOpen(open);
+    if (!open) {
+      setSearch('');
+      setPage(1);
+    }
+  }
+
+  function addPassport(code: string) {
+    setPassportSearchOpen(false);
+    setHasPassport((prev) => ({
+      ...prev,
+      [code]: true,
+    }));
+  }
+
+  function removePassport(code: string) {
+    setHasPassport((prev) => {
+      const newPassports = { ...prev };
+      delete newPassports[code];
+      return newPassports;
+    });
+  }
+
   return (
     <>
-      <div>Passport Checker</div>
+      <AlertDialog open={passportSearchOpen} onOpenChange={handleOpenChange}>
+        <AlertDialogTrigger asChild>
+          <Button variant="secondary" style={{ fontWeight: 500 }}>
+            <Plus /> ADD PASSPORT
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent className={styles.modal_container}>
+          <AlertDialogHeader>
+            <div className="flex items-center justify-between">
+              <AlertDialogTitle style={{ fontSize: '1.3rem' }}>
+                Add a passport
+              </AlertDialogTitle>
+              <AlertDialogCancel asChild>
+                <Button
+                  variant="ghost"
+                  elevated={false}
+                  size="icon"
+                  style={{ borderWidth: 0 }}
+                >
+                  <X />
+                </Button>
+              </AlertDialogCancel>
+            </div>
+          </AlertDialogHeader>
+          <AlertDialogDescription style={{ display: 'none' }}>
+            Search for a passport to check its passport index.
+          </AlertDialogDescription>
+          <div className={styles.modal_content}>
+            <Input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              startIcon={Search}
+              placeholder="Search for a country..."
+            />
+
+            <div className={styles.countries}>
+              {countries.map((country) => (
+                <Button
+                  key={country.code}
+                  variant="outline"
+                  elevated={false}
+                  className="w-full h-[120px] relative"
+                  title={country.name}
+                  onClick={() => addPassport(country.code)}
+                >
+                  <div className={styles.country_container}>
+                    {hasPassport[country.code] && (
+                      <div className={styles.check}>
+                        <Check strokeWidth="3px" size="28px" />
+                      </div>
+                    )}
+                    <div className="w-4/5">
+                      <AspectRatio ratio={4 / 3} className="bg-muted">
+                        <Image
+                          src={`https://flagcdn.com/${country.code.toLowerCase()}.svg`}
+                          fill
+                          alt={country.name}
+                          className="h-full w-full rounded-md object-contain"
+                        />
+                      </AspectRatio>
+                    </div>
+                    <p>{country.name}</p>
+                  </div>
+                </Button>
+              ))}
+            </div>
+
+            <Pagination page={page} pageCount={pageCount} setPage={setPage} />
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 }
