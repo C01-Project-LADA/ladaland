edfc5b2a0b89caebebf287e762fe0017bb37f44e
Andre Fong <fongandre123@gmail.com>
2025-02-18 00:46:21 -0500

    chore: Add test globe

--- client/src/app/(authenticated)/home/page.tsx
+++ client/src/app/(authenticated)/home/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+'use client';
+
+import Globe from 'globe.gl';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
-  return (
-    <div>
-      <div>Home</div>
-    </div>
-  );
+  const globe = useRef(null);
+
+  useEffect(() => {
+    if (!globe.current) return;
+
+    const myGlobe = new Globe(globe.current)
+      .width(500)
+      .height(500)
+      .backgroundColor('#f5f5f5');
+  }, []);
+
+  return <div id="globeViz" ref={globe} className="max-w-[500px]"></div>;
 }
