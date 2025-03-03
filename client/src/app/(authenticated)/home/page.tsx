c6644659785d2d343efafbda3476e63ae0a3bebe
Elizabeth <eilina4162@gmail.com>
2025-03-02 19:56:01 -0500

    [LADA-41] - create banner to show % of countries visited

--- client/src/app/(authenticated)/home/page.tsx
+++ client/src/app/(authenticated)/home/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import VisitedCountriesBanner from '@/components/VisitedCountriesBanner';
 
 export default function Home() {
   return (
     <div>
+      <VisitedCountriesBanner />
       <div>Home</div>
     </div>
   );
