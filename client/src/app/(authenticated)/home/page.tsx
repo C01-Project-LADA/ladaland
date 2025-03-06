65512f8780fef7251975d5fdc4918d92c6749932
Andre Fong <fongandre123@gmail.com>
2025-03-05 20:50:40 -0500

    feat: Mark countries as visited

--- client/src/app/(authenticated)/home/page.tsx
+++ client/src/app/(authenticated)/home/page.tsx
@@ -3,6 +3,7 @@
 import dynamic from 'next/dynamic';
 const Globe = dynamic(() => import('react-globe.gl'), { ssr: false });
 import {
+  useCallback,
   // useCallback,
   useState,
 } from 'react';
@@ -16,6 +17,29 @@ export default function Home() {
   const [hoverD, setHoverD] = useState<GlobeCountry>();
   // const currentCountryCode = hoverD?.properties?.ISO_A2;
 
+  const [visitedCountries, setVisitedCountries] = useState<string[]>([]);
+  const getPolygonCapColor = useCallback(
+    (d: GlobeCountry) => {
+      if (visitedCountries.includes(d.properties.ISO_A2)) {
+        return 'rgba(61, 255, 39, 0.7)';
+      }
+      return 'rgba(14, 143, 0, 0.7)';
+    },
+    [visitedCountries]
+  );
+
+  const handleRightClick = useCallback(
+    (d: GlobeCountry) => {
+      const countryCode = d.properties.ISO_A2;
+      if (visitedCountries.includes(countryCode)) {
+        setVisitedCountries((prev) => prev.filter((c) => c !== countryCode));
+      } else {
+        setVisitedCountries((prev) => [...prev, countryCode]);
+      }
+    },
+    [visitedCountries]
+  );
+
   return (
     <div id="globe">
       <Globe
@@ -27,7 +51,8 @@ export default function Home() {
         polygonAltitude={(d) => (d === hoverD ? 0.03 : 0.01)}
         polygonStrokeColor={() => '#111'}
         // TODO: Adjust color based on visited or not
-        polygonCapColor={() => 'rgba(20, 198, 0, 0.7)'}
+        // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
+        polygonCapColor={getPolygonCapColor}
         polygonSideColor={() => '#14c600'}
         // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
         onPolygonHover={setHoverD}
@@ -36,25 +61,35 @@ export default function Home() {
           console.log(polygon.properties.ISO_A2);
         }}
         // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
+        onPolygonRightClick={handleRightClick}
+        // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
         polygonLabel={({ properties: d }) => (
-          <div className="p-2 w-fit flex gap-2">
-            <div className="flex-[3]">
-              <p className="text-xl font-bold leading-none">{d.ADMIN}</p>
-              <p className="text-xs mt-1 font-light text-gray-300">
-                {(d.CONTINENT as string).toUpperCase()}
-              </p>
-              <p className="text-sm mt-2 font-light leading-none">
-                Click to plan your next plan trip here!
-              </p>
-              {/* <p>Visited 09/24/2024</p> */}
+          <div className="p-2 w-fit">
+            <div className="flex gap-2 mb-2">
+              <div className="flex-[3]">
+                <p className="text-xl font-bold leading-none">{d.ADMIN}</p>
+                <p className="text-xs mt-1 font-light text-gray-300">
+                  {(d.CONTINENT as string).toUpperCase()}
+                </p>
+              </div>
+              <div className="flex-1">
+                <span
+                  className={`fi !w-full aspect-[4/3] fi-${(
+                    d.ISO_A2 as string
+                  ).toLowerCase()} rounded-md`}
+                />
+              </div>
             </div>
-            <div className="flex-1">
-              <span
-                className={`fi !w-full aspect-[4/3] fi-${(
-                  d.ISO_A2 as string
-                ).toLowerCase()} rounded-md`}
-              />
-            </div>
+
+            <p className="text-xs mt-1 font-light leading-none">
+              Click to plan your next trip here!
+            </p>
+            <p className="text-xs mt-2 font-light leading-none">
+              {visitedCountries.includes(d.ISO_A2 as string)
+                ? 'Marked as visited'
+                : 'Right click to mark as visited!'}
+            </p>
+            {/* <p>Visited 09/24/2024</p> */}
           </div>
         )}
         polygonsTransitionDuration={300}
