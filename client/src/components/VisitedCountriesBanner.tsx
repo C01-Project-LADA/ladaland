f1c699ea1a1ed9f6fcc138f81ffc49390e8501a4
Elizabeth Ilina <133707687+elizabeth-ilina@users.noreply.github.com>
2025-03-05 23:01:19 -0500

    [LADA-54] - connect core and client for marking visited countries (#37)
    Also slightly updated showing % of visited countries logic

--- client/src/components/VisitedCountriesBanner.tsx
+++ client/src/components/VisitedCountriesBanner.tsx
@@ -1,37 +1,17 @@
 "use client";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "@/styles/VisitedCountriesBanner.module.css";
 
-export default function VisitedCountriesBanner() {
-  const [visitedMessage, setVisitedMessage] = useState<string | null>(null);
+interface VisitedCountriesBannerProps {
+  visitedMessage: string;
+}
 
-  useEffect(() => {
-    async function fetchVisitedPercentage() {
-      try {
-        const response = await axios.get("http://localhost:4000/api/visitedCountriesPercent", {
-          withCredentials: true,
-        });
-
-        if (response.status === 200) {
-          setVisitedMessage(response.data.message);
-        } else {
-          setVisitedMessage("Unable to get visited countries data.");
-        }
-      } catch (error) {
-        console.error("Error getting visited countries percent:", error);
-        setVisitedMessage("Error loading travel progress.");
-      }
-    }
-
-    fetchVisitedPercentage();
-  }, []);
-
+export default function VisitedCountriesBanner({ visitedMessage }: VisitedCountriesBannerProps) {
   return (
     <div className={styles.container}>
       <div className={styles.header_block}>
         <h2>Travel Progress</h2>
-        <h1>{visitedMessage ?? "Loading your travel progress..."}</h1>
+        <h1>{visitedMessage}</h1>
       </div>
     </div>
   );
