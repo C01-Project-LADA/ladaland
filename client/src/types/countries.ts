2ec6570c45be760513de5543f82844f39bd1871a
Andre Fong <99469779+andre-fong@users.noreply.github.com>
2025-03-05 21:33:07 -0500

    [LADA-37] Mark Visited Countries on a Map (#35)
    ### DONE:
    
    - Add interactive 3D globe to home page
    - Add ability to mark countries with a right click
    - Add country label on hover with some info
    - Fix some compatibility issues with globe.gl and preact (fixed in
    .patch file)
    
    ### TODO/BLOCKED:
    
    - Connect with backend
    - Potentially some wording changes
    - Redirect to plan a new trip page when left clicking a country
    - Possibly better visual bounds for the globe interactive area (?)
    - Possibly more info to display on hover (?)
    
    ### INFO:
    
    - Lots of shit to npm i!

--- client/src/types/countries.ts
+++ client/src/types/countries.ts
@@ -4,3 +4,17 @@ type Country = {
   name: string;
   code: string;
 };
+
+type GlobeCountry = {
+  type: string;
+  properties: GlobeCountryProperties;
+};
+
+type GlobeCountryProperties = {
+  ABBREV: string;
+  FORMAL_EN: string;
+  CONTINENT: string;
+  ISO_A2: string;
+  ISO_A3: string;
+  NAME: string;
+};
