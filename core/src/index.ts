2bbce02b1ff8f4a1c49fd7721f89e4c0e9db130c
Andrey Zinovyev <74675733+AndreyZIno@users.noreply.github.com>
2025-03-06 16:15:55 -0500

    [LADA-68] [Core] - Implement logout (#43)
    Co-authored-by: Daniel Alyoshin <daniel.alyoshin@gmail.com>

--- core/src/index.ts
+++ core/src/index.ts
@@ -14,6 +14,7 @@ import markVistedCountriesRoute from './routes/markVistedCountries';
 import meRoute from './routes/user/me';
 import postsRoute from './routes/posts';
 import postVotesRoute from "./routes/post-votes";
+import logoutRoute from "./routes/user/logout";
 
 dotenv.config();
 
@@ -48,7 +49,8 @@ app.use('/api', travelSuggestionsRoute);
 app.use('/api', markVistedCountriesRoute);
 app.use('/api', meRoute);
 app.use('/api/posts', postsRoute);
-app.use("/api/post-votes", postVotesRoute);
+app.use('/api/post-votes', postVotesRoute);
+app.use('/api', logoutRoute);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
