a523b6061c4284452466adbfdbb06d90ce818bc5
Andrey Zinovyev <andrey.znvyev@gmail.com>
2025-03-06 15:44:38 -0500

    Implement logout

--- core/src/index.ts
+++ core/src/index.ts
@@ -14,6 +14,7 @@ import markVistedCountriesRoute from './routes/markVistedCountries';
 import meRoute from './routes/user/me';
 import postsRoute from './routes/posts';
 import commentsRoute from './routes/comments';
+import logoutRoute from './routes/user/logout';
 
 dotenv.config();
 
@@ -49,6 +50,7 @@ app.use('/api', markVistedCountriesRoute);
 app.use('/api', meRoute);
 app.use('/api/posts', postsRoute); 
 app.use('/api/comments', commentsRoute);
+app.use('/api', logoutRoute);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
