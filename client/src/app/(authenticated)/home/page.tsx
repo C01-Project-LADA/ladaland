c2f8a13a1ea83833b94e0a6a7c86440d6b5031fc
Andre Fong <fongandre123@gmail.com>
2025-03-06 15:23:03 -0500

    chore: Modify home page styling with globe

--- client/src/app/(authenticated)/home/page.tsx
+++ client/src/app/(authenticated)/home/page.tsx
@@ -22,8 +22,12 @@ export default function Home() {
     async function fetchVisitedData() {
       try {
         const [countriesResponse, percentResponse] = await Promise.all([
-          axios.get('http://localhost:4000/api/getVisitedCountries', { withCredentials: true }),
-          axios.get('http://localhost:4000/api/visitedCountriesPercent', { withCredentials: true }),
+          axios.get('http://localhost:4000/api/getVisitedCountries', {
+            withCredentials: true,
+          }),
+          axios.get('http://localhost:4000/api/visitedCountriesPercent', {
+            withCredentials: true,
+          }),
         ]);
 
         if (countriesResponse.status === 200) {
@@ -71,7 +75,10 @@ export default function Home() {
         if (response.status === 200) {
           setVisitedCountries(response.data.visitedCountries);
 
-          const percentResponse = await axios.get('http://localhost:4000/api/visitedCountriesPercent', { withCredentials: true });
+          const percentResponse = await axios.get(
+            'http://localhost:4000/api/visitedCountriesPercent',
+            { withCredentials: true }
+          );
           if (percentResponse.status === 200) {
             setVisitedMessage(percentResponse.data.message);
           }
@@ -84,58 +91,60 @@ export default function Home() {
   );
 
   return (
-    <div id="globe">
+    <div>
       <VisitedCountriesBanner visitedMessage={visitedMessage} />
-      <Globe
-        width={500}
-        height={height}
-        backgroundColor="#f5f5f5"
-        globeImageUrl="https://unpkg.com/three-globe/example/img/earth-night.jpg"
-        polygonsData={countries.features}
-        polygonAltitude={(d) => (d === hoverD ? 0.03 : 0.01)}
-        polygonStrokeColor={() => '#111'}
-        // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
-        polygonCapColor={getPolygonCapColor}
-        polygonSideColor={() => '#14c600'}
-        // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
-        onPolygonHover={setHoverD}
-        onPolygonClick={(polygon) => {
+      <div id="globe" className="-mt-10">
+        <Globe
+          width={500}
+          height={height - 100}
+          backgroundColor="#f5f5f5"
+          globeImageUrl="https://unpkg.com/three-globe/example/img/earth-night.jpg"
+          polygonsData={countries.features}
+          polygonAltitude={(d) => (d === hoverD ? 0.03 : 0.01)}
+          polygonStrokeColor={() => '#111'}
           // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
-          console.log(polygon.properties.ISO_A2);
-        }}
-        // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
-        onPolygonRightClick={handleRightClick}
-        // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
-        polygonLabel={({ properties: d }) => (
-          <div className="p-2 w-fit">
-            <div className="flex gap-2 mb-2">
-              <div className="flex-[3]">
-                <p className="text-xl font-bold leading-none">{d.ADMIN}</p>
-                <p className="text-xs mt-1 font-light text-gray-300">
-                  {(d.CONTINENT as string).toUpperCase()}
-                </p>
+          polygonCapColor={getPolygonCapColor}
+          polygonSideColor={() => '#14c600'}
+          // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
+          onPolygonHover={setHoverD}
+          onPolygonClick={(polygon) => {
+            // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
+            console.log(polygon.properties.ISO_A2);
+          }}
+          // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
+          onPolygonRightClick={handleRightClick}
+          // @ts-expect-error: react-globe.gl has terrible ts support, SAD!
+          polygonLabel={({ properties: d }) => (
+            <div className="p-2 w-fit">
+              <div className="flex gap-2 mb-2">
+                <div className="flex-[3]">
+                  <p className="text-xl font-bold leading-none">{d.ADMIN}</p>
+                  <p className="text-xs mt-1 font-light text-gray-300">
+                    {(d.CONTINENT as string).toUpperCase()}
+                  </p>
+                </div>
+                <div className="flex-1">
+                  <span
+                    className={`fi !w-full aspect-[4/3] fi-${(
+                      d.ISO_A2 as string
+                    ).toLowerCase()} rounded-md`}
+                  />
+                </div>
               </div>
-              <div className="flex-1">
-                <span
-                  className={`fi !w-full aspect-[4/3] fi-${(
-                    d.ISO_A2 as string
-                  ).toLowerCase()} rounded-md`}
-                />
-              </div>
-            </div>
 
-            <p className="text-xs mt-1 font-light leading-none">
-              Click to plan your next trip here!
-            </p>
-            <p className="text-xs mt-2 font-light leading-none">
-              {visitedCountries.includes(d.ISO_A2 as string)
-                ? 'Marked as visited'
-                : 'Right click to mark as visited!'}
-            </p>
-          </div>
-        )}
-        polygonsTransitionDuration={300}
-      />
+              <p className="text-xs mt-1 font-light leading-none">
+                Click to plan your next trip here!
+              </p>
+              <p className="text-xs mt-2 font-light leading-none">
+                {visitedCountries.includes(d.ISO_A2 as string)
+                  ? 'Marked as visited'
+                  : 'Right click to mark as visited!'}
+              </p>
+            </div>
+          )}
+          polygonsTransitionDuration={300}
+        />
+      </div>
     </div>
   );
 }
