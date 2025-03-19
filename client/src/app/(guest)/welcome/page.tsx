1677321bb04e0b16f3651ae9bd43d94fe1305756
Andre Fong <fongandre123@gmail.com>
2025-03-18 20:55:08 -0400

    chore: Add homepage skeleton design

--- client/src/app/(guest)/welcome/page.tsx
+++ client/src/app/(guest)/welcome/page.tsx
@@ -1,10 +1,36 @@
+import { Button } from '@/components/ui/button';
+import HomeGlobe from '@/components/HomeGlobe';
+import SlidingCountries from '@/components/SlidingCountries';
+
 export default function Welcome() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        (DEV) If you&apos;re seeing this page, try logging in or setting the
-        cookie &quot;connect.sid&quot; to see the home page.
-      </main>
-    </div>
+    <>
+      <div className="items-center flex px-[15%]">
+        <div className="flex-[1.5]">
+          <HomeGlobe />
+        </div>
+        <div className="flex-1">
+          <h1 className="mt-16 text-2xl font-bold">
+            Gameify your travels, track your budget, and build your itinerary —
+            one trip at a time!
+          </h1>
+
+          <div className="mt-8 flex justify-center">
+            <Button className="px-36 max-w-24">GET STARTED</Button>
+          </div>
+          <div className="mt-6 flex justify-center">
+            <Button
+              variant="outline"
+              className="px-36 font-bold max-w-24"
+              style={{ color: 'var(--lada-accent)' }}
+            >
+              I ALREADY HAVE AN ACCOUNT
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <SlidingCountries />
+    </>
   );
 }
---
+++ client/src/components/HomeGlobe.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function HomeGlobe() {
+  return (
+    <div>
+      <div>HomeGlobe</div>
+    </div>
+  );
+}
