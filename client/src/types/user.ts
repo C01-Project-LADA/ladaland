19d53adab9f2fc782157f1eab9a303c36476759d
Elizabeth Ilina <133707687+elizabeth-ilina@users.noreply.github.com>
2025-03-20 15:09:47 -0400

    [LADA-61] (#57)
    ### DONE:
    
    - toast shows up when user gained points for making a post
    - user's xp gets increased when post is made + decreased when post is
    deleted in the frontend
    
    ### TODO/BLOCKED:
    
    - To see the update of xp, user needs to refresh the page. TODO: show
    the update to xp as soon as user makes the post so they don't have to
    refresh the page.
    
    ### INFO:
    
    -

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,9 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+ /* eslint-disable @typescript-eslint/no-unused-vars */
 
-type User = {
+export type User = {
   id: string;
   username: string;
   email: string;
+  points: number;
+  level?: number;
   phone: string;
   createdAt: string;
 };
