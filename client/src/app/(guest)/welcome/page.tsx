3f2113e865665370783fc07ad93b5fc65b8372a7
Andre Fong <fongandre123@gmail.com>
2025-03-20 17:16:58 -0400

    chore: Add sliding countries on welcome page

--- client/src/app/(guest)/welcome/page.tsx
+++ client/src/app/(guest)/welcome/page.tsx
@@ -1,28 +1,41 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import HomeGlobe from '@/components/HomeGlobe';
 import SlidingCountries from '@/components/SlidingCountries';
 
 export default function Welcome() {
+  function handleGetStarted() {
+    document.getElementById('get-started')?.click();
+  }
+
+  function handleHaveAccount() {
+    document.getElementById('log-in')?.click();
+  }
+
   return (
     <>
-      <div className="items-center flex px-[15%] gap-5 h-full">
+      <div className="items-center flex px-[15%] gap-5">
         <div className="flex-[1.5] mt-5 basis-2/3">
           <HomeGlobe />
         </div>
         <div className="flex-1 basis-1/3">
-          <h1 className="mt-12 text-2xl font-bold">
+          <h1 className="mt-16 text-2xl font-bold">
             Gameify your travels, track your budget, and build your itinerary —
             one trip at a time!
           </h1>
 
           <div className="mt-8 flex justify-center">
-            <Button className="px-36 max-w-24">GET STARTED</Button>
+            <Button className="px-36 max-w-24" onClick={handleGetStarted}>
+              GET STARTED
+            </Button>
           </div>
           <div className="mt-6 flex justify-center">
             <Button
               variant="outline"
               className="px-36 font-bold max-w-24"
               style={{ color: 'var(--lada-accent)' }}
+              onClick={handleHaveAccount}
             >
               I ALREADY HAVE AN ACCOUNT
             </Button>
--- client/src/components/SlidingCountries.tsx
+++ client/src/components/SlidingCountries.tsx
@@ -1,9 +1,66 @@
 'use client';
 
+import styles from '@/styles/SlidingCountries.module.css';
+import ct from 'i18n-iso-countries';
+import en from 'i18n-iso-countries/langs/en.json';
+
+ct.registerLocale(en);
+
+const scrollingCountries = [
+  'mx',
+  'gt',
+  'hn',
+  'ni',
+  'cr',
+  'pa',
+  'cu',
+  'ca',
+  'us',
+  'jm',
+  'ht',
+  'do',
+  'bs',
+  'bb',
+  'gd',
+  'tt',
+  'lc',
+  'kn',
+  'ag',
+  'vc',
+  'dm',
+  've',
+  'kr',
+];
+
 export default function SlidingCountries() {
   return (
-    <div>
-      <div>SlidingCountries</div>
+    <div className="border-t border-b border-gray-400 py-4 mt-16 flex overflow-hidden">
+      <div className={`flex gap-8 ${styles.scroll1}`}>
+        {scrollingCountries.map((country) => (
+          <div className="flex items-center gap-1.5 w-fit" key={country}>
+            <span
+              className={`fi !w-[35px] max-w-[80px] aspect-[4/3] fi-${country} bg-muted rounded-md`}
+            />
+            <p className="text-gray-800 text-sm leading-none text-xs w-max">
+              {ct.getName(country, 'en')}
+            </p>
+          </div>
+        ))}
+        <div />
+      </div>
+      <div className={`flex gap-8 ${styles.scroll2}`}>
+        {scrollingCountries.map((country) => (
+          <div className="flex items-center gap-1.5 w-fit" key={country}>
+            <span
+              className={`fi !w-[35px] max-w-[80px] aspect-[4/3] fi-${country} bg-muted rounded-md`}
+            />
+            <p className="text-gray-800 text-sm leading-none text-xs w-max">
+              {ct.getName(country, 'en')}
+            </p>
+          </div>
+        ))}
+        <div />
+      </div>
     </div>
   );
 }
