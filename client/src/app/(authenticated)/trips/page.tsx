95cd9a126d43bf8f7aa7bf9a1d6b25e500ad0799
Andre Fong <fongandre123@gmail.com>
2025-03-21 21:54:46 -0400

    chore: New trip button functionality

--- client/src/app/(authenticated)/trips/page.tsx
+++ client/src/app/(authenticated)/trips/page.tsx
@@ -5,6 +5,9 @@ import Trip from '@/components/Trip';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { Plus } from 'lucide-react';
+import CountrySelectDialog from '@/components/CountrySelectDialog';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const mockTrip: Trip = {
   id: '1',
@@ -18,6 +21,21 @@ const mockTrip: Trip = {
 };
 
 export default function Trips() {
+  const router = useRouter();
+
+  const [countriesSelected, setCountriesSelected] = useState<
+    Record<string, Country>
+  >({});
+
+  // When countries selected changes, extract the country selected and revert it back to an empty object
+  useEffect(() => {
+    if (Object.keys(countriesSelected).length > 0) {
+      const country = Object.values(countriesSelected)[0];
+      setCountriesSelected({});
+      router.push(`/trips/new?country=${country.code}`);
+    }
+  }, [countriesSelected, router]);
+
   return (
     <div
       className="pl-[20px] pt-[30px]"
@@ -32,13 +50,21 @@ export default function Trips() {
           <div className="mt-1">
             <p>Plan trips and level up!</p>
             <div className="mt-5 mb-1">
-              <Button
-                variant="outline"
-                className="font-bold"
-                style={{ color: 'var(--lada-primary)' }}
-              >
-                <Plus /> NEW TRIP
-              </Button>
+              <CountrySelectDialog
+                title="Plan a new trip to..."
+                description="Select a country to plan a new trip to."
+                countriesSelected={{}}
+                setCountriesSelected={setCountriesSelected}
+                dialogTrigger={
+                  <Button
+                    variant="outline"
+                    className="font-bold"
+                    style={{ color: 'var(--lada-primary)' }}
+                  >
+                    <Plus /> NEW TRIP
+                  </Button>
+                }
+              />
             </div>
           </div>
         }
