fda410e697947ae2f32bf7bfbca93d820606e74b
danielalyoshin <daniel.alyoshin@gmail.com>
2025-03-23 15:10:02 -0400

    Implement

--- core/src/tests/login.test.ts
+++ core/src/tests/login.test.ts
@@ -96,12 +96,10 @@ describe('POST /api/login', () => {
       password: 'hashedpassword',
     });
     compareMock.mockResolvedValueOnce(true);
-    const res = await request(app)
-      .post('/api/login')
-      .send({
-        usernameOrEmail: 'test2@example.com',
-        password: 'correctpassword',
-      });
+    const res = await request(app).post('/api/login').send({
+      usernameOrEmail: 'test2@example.com',
+      password: 'correctpassword',
+    });
     expect(res.status).toBe(200);
     expect(res.body.message).toBe('Login successful');
   });
