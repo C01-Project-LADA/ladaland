e1e7cb4ba70c2a7ade27b2c47e4cc07981dba8b1
danielalyoshin <daniel.alyoshin@gmail.com>
2025-03-24 09:41:47 -0400

    check

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,13 +1,9 @@
 import dotenv from 'dotenv';
-import http from 'http';
 import app from './app';
 
 dotenv.config();
 
-const port = 4000;
-
-const server = http.createServer({ keepAliveTimeout: 700_000 }, app);
-
-server.listen(port, '0.0.0.0', () => {
-  console.log(`Core server is running on http://0.0.0.0:${port}`);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Core server is running on http://localhost:${port}`);
 });
