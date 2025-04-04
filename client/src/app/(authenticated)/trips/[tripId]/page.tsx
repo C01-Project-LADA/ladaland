181f2eb4cbdad2ecf0d7dff648dfe19e42970c60
Andre Fong <fongandre123@gmail.com>
2025-04-04 16:11:08 -0400

    chore: Add edit trip page skeleton

---
+++ client/src/app/(authenticated)/trips/[tripId]/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { Suspense, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import NewTripForm from '@/components/NewTripForm';
+import PageBanner from '@/components/PageBanner';
+import { toast } from 'sonner';
+import useTripForm from '@/hooks/useTripForm';
+
+function ExistingTripContent() {
+  const searchParams = useSearchParams();
+  const { submitting, handleSubmit, error, clearError } = useTripForm(true);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+  }, [error]);
+
+  const country = searchParams.get('country');
+
+  async function handleTripSubmit(
+    tripData: Omit<Trip, 'expenses' | 'id'> & {
+      expenses: (Omit<Expense, 'id'> & { id?: string })[];
+    }
+  ) {
+    clearError();
+    await handleSubmit(tripData);
+  }
+
+  return (
+    <div
+      className="pl-[20px] pt-[30px]"
+      style={{ width: 'clamp(200px, 50vw, 500px)' }}
+    >
+      <PageBanner
+        title="ALL TRIPS"
+        message="Edit your trip"
+        variant="blue"
+        backLink="/trips"
+      />
+      <NewTripForm
+        submitting={submitting}
+        loadedLocation={country || ''}
+        onSubmit={handleTripSubmit}
+      />
+    </div>
+  );
+}
+
+export default function ExistingTripPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ExistingTripContent />
+    </Suspense>
+  );
+}
