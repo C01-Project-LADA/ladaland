137ceb453c7a7ad6c75896eff39851c2e8a9a564
Andre Fong <99469779+andre-fong@users.noreply.github.com>
2025-04-04 22:28:22 -0400

    [LADA-104, LADA-40] Trip Page AI + Edit Trip (#98)
    ### DONE:
    
    - Edit Trip page
    - AI suggestions button
    - Change favicon to transparent ver.
    - Make side container collapse earlier
    
    ### TODO/BLOCKED:
    
    - TEST TEST TEST
    
    ### INFO:
    
    -

---
+++ client/src/app/(authenticated)/trips/[tripId]/page.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { Suspense, useEffect } from 'react';
+import { useParams, useSearchParams } from 'next/navigation';
+import NewTripForm from '@/components/NewTripForm';
+import PageBanner from '@/components/PageBanner';
+import { toast } from 'sonner';
+import useTripForm from '@/hooks/useTripForm';
+import useTrip from '@/hooks/useTrip';
+
+function ExistingTripContent() {
+  const searchParams = useSearchParams();
+  const { tripId } = useParams();
+  const { submitting, handleSubmit, error, clearError, deleteExpense } =
+    useTripForm();
+  const {
+    trip,
+    // loading,
+    // error: getTripError,
+    // deleteTrip,
+    // toggleCompleteTrip,
+  } = useTrip((tripId as string) || '');
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+  }, [error]);
+
+  const country = searchParams.get('country');
+
+  async function handleTripSubmit(
+    tripData: Omit<Trip, 'expenses' | 'id'> & {
+      expenses: (Omit<Expense, 'id'> & { id?: string })[];
+    }
+  ) {
+    clearError();
+    await handleSubmit(tripData);
+  }
+
+  return (
+    <div
+      className="pl-[20px] pt-[30px]"
+      style={{ width: 'clamp(200px, 50vw, 500px)' }}
+    >
+      <PageBanner
+        title="ALL TRIPS"
+        message="Edit your trip"
+        variant="blue"
+        backLink="/trips"
+      />
+      <NewTripForm
+        isNewTrip={false}
+        existingTrip={trip || undefined}
+        deleteExpense={deleteExpense}
+        submitting={submitting}
+        loadedLocation={country || ''}
+        onSubmit={handleTripSubmit}
+      />
+    </div>
+  );
+}
+
+export default function ExistingTripPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ExistingTripContent />
+    </Suspense>
+  );
+}
